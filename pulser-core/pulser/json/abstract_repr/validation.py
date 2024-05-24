e3d13cfde4c7b9f95489b7599cf02e38e0802080
Amin <mohamed.mallek@pasqal.com>
2024-05-24 16:13:13 +0200

    use fastjsonschema

--- pulser-core/pulser/json/abstract_repr/validation.py
+++ pulser-core/pulser/json/abstract_repr/validation.py
@@ -13,99 +13,50 @@
 # limitations under the License.
 """Function for validation of JSON serialization to abstract representation."""
 import json
-import os
 from typing import Literal
 
-from pulser.json.abstract_repr import SCHEMAS, SCHEMAS_PATH
+from pulser.json.abstract_repr import SCHEMAS
+import fastjsonschema
 
-USE_FASTJSONSCHEMA = (
-    os.environ.get("USE_FASTJSONSCHEMA", "False").lower() == "true"
-)
+registry = {
+    "device-schema.json": SCHEMAS["device"]["definitions"],
+    "layout-schema.json": SCHEMAS["layout"]["definitions"],
+    "register-schema.json": SCHEMAS["register"]["definitions"],
+    "noise-schema.json": SCHEMAS["noise"],
+}
 
-if USE_FASTJSONSCHEMA:
-    import fastjsonschema
-
-    registry = {
-        "device-schema.json": SCHEMAS["device"]["definitions"],
-        "layout-schema.json": SCHEMAS["layout"]["definitions"],
-        "register-schema.json": SCHEMAS["register"]["definitions"],
-        "noise-schema.json": SCHEMAS["noise"],
-    }
-
-    def resolve_references(schema, registry):
-        if isinstance(schema, dict):
-            if "$ref" in schema and schema["$ref"] in registry:
-                v = schema.pop("$ref")
-                schema.update(**registry[v])
-                return schema
-            else:
-                return {
-                    k: resolve_references(v, registry)
-                    for k, v in schema.items()
-                }
-        elif isinstance(schema, list):
-            return [resolve_references(item, registry) for item in schema]
-        else:
+def resolve_references(schema, registry):
+    if isinstance(schema, dict):
+        if "$ref" in schema and schema["$ref"] in registry:
+            v = schema.pop("$ref")
+            schema.update(**registry[v])
             return schema
+        else:
+            return {
+                k: resolve_references(v, registry)
+                for k, v in schema.items()
+            }
+    elif isinstance(schema, list):
+        return [resolve_references(item, registry) for item in schema]
+    else:
+        return schema
 
-    VALIDATORS = {
-        name: fastjsonschema.compile(
-            resolve_references(SCHEMAS[name], registry)
-        )
-        for name in ["sequence", "device", "layout", "register", "noise"]
-    }
-
-    def validate_abstract_repr(
-        obj_str: str,
-        name: Literal["sequence", "device", "layout", "register", "noise"],
-    ) -> None:
-        """Validate the abstract representation of an object.
-
-        Args:
-            obj_str: A JSON-formatted string encoding the object.
-            name: The type of object to validate (can be "sequence" or
-            "device").
-        """
-        VALIDATORS[name](json.loads(obj_str))
-
-else:
-    from importlib.metadata import version
-
-    import jsonschema
-    from referencing import Registry, Resource
-
-    LEGACY_JSONSCHEMA = "4.18" > version("jsonschema") >= "4.17.3"
-
-    REGISTRY: Registry = Registry(
-        [
-            ("device-schema.json", Resource.from_contents(SCHEMAS["device"])),
-            ("layout-schema.json", Resource.from_contents(SCHEMAS["layout"])),
-            (
-                "register-schema.json",
-                Resource.from_contents(SCHEMAS["register"]),
-            ),
-            ("noise-schema.json", Resource.from_contents(SCHEMAS["noise"])),
-        ]
+VALIDATORS = {
+    name: fastjsonschema.compile(
+        resolve_references(SCHEMAS[name], registry)
     )
+    for name in ["sequence", "device", "layout", "register", "noise"]
+}
 
-    def validate_abstract_repr(
-        obj_str: str,
-        name: Literal["sequence", "device", "layout", "register", "noise"],
-    ) -> None:
-        """Validate the abstract representation of an object.
+def validate_abstract_repr(
+    obj_str: str,
+    name: Literal["sequence", "device", "layout", "register", "noise"],
+) -> None:
+    """Validate the abstract representation of an object.
 
-        Args:
-            obj_str: A JSON-formatted string encoding the object.
-            name: The type of object to validate (can be "sequence" or
-            "device").
-        """
-        obj = json.loads(obj_str)
-        validate_args = dict(instance=obj, schema=SCHEMAS[name])
-        if LEGACY_JSONSCHEMA:  # pragma: no cover
-            validate_args["resolver"] = jsonschema.validators.RefResolver(
-                base_uri=f"{SCHEMAS_PATH.resolve().as_uri()}/",
-                referrer=SCHEMAS[name],
-            )
-        else:  # pragma: no cover
-            validate_args["registry"] = REGISTRY
-        jsonschema.validate(**validate_args)
+    Args:
+        obj_str: A JSON-formatted string encoding the object.
+        name: The type of object to validate (can be "sequence" or
+        "device").
+    """
+    VALIDATORS[name](json.loads(obj_str))
